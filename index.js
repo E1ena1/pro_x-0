a3aff4dc26a2a3f7c12d5406619d459b59a6933a
ElenaK <kalinakrasnaya33@gmail.com>
2022-07-16 10:52:21 +0300

    add checkWinner

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 let currentPlayer = "0";
 let numberOfPlayers = 2;
-let won = true;
+let won = false;
 
-//------------//
-//document.getElementById("player").innerText = "Kraken"
-//------------//
-//function place(box) {
-	//if (box.innerText != "") return;
-	//box.innerText = currentPlayer;
-		//if (currentPlayer == "0") {
-			//currentPlayer = "X";
-		//}
-		//else {
-			//currentPlayer = "0";
-		//}
-//}
 
 function place(box) {
-	if (box.innerText != "") return;
+	if (box.innerText != "" || won) return;
 	box.innerText = currentPlayer;
 	currentPlayer == "0" ? currentPlayer = "X" : currentPlayer = "0";
+	checkGameBoard();
+}
+
+function checkGameBoard() {
+	for(let i = 0; i <= 2; i++) {
+		checkWinner(document.getElementById(i + "_0").innerText,
+			document.getElementById(i + "_1").innerText,
+			document.getElementById(i + "_2").innerText);
+		checkWinner(document.getElementById("0_" + i).innerText,
+			document.getElementById("1_" + i).innerText,
+			document.getElementById("2_" + i).innerText);
+	}
+	checkWinner(document.getElementById("0_0").innerText,
+		document.getElementById("1_1").innerText,
+		document.getElementById("2_2").innerText);
+	checkWinner(document.getElementById("0_2").innerText,
+		document.getElementById("1_1").innerText,
+		document.getElementById("2_0").innerText);
+}
+function checkWinner (first, second, third) {
+	if (first != "" && first == second && first == third) {
+		won = true;
+		document.body.style.background = "blue";
+	}
 }
